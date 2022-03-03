05a922b2eaf8edaa7a4db2fc9a12bcc15a4ad5cd
fl1k <vojin.golubovic97@gmail.com>
2022-03-04 00:15:31 +0100

    Flower i flowers

---
+++ flower.js
@@ -0,0 +1,20 @@
+class Flower
+{
+    constructor(id, waterNeeded, lifetime, reward)
+    {
+        this.id = id;
+        this.waterNeeded = waterNeeded;
+        this.lifetime = lifetime;
+        this.reward = reward;
+    }
+}
+
+const flowers =
+{
+    anemone: new Flower(3, 2, 5, 2000),
+    blueJazz: new Flower(4, 2, 4, 2500),
+    crocus: new Flower(5, 5, 4, 5000),
+    tulip: new Flower(6, 1, 3, 8000),
+}
+
+export {Flower, flowers};
