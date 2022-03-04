224046cabe224f7c8faee4cc19e83f275458819c
fl1k <vojin.golubovic97@gmail.com>
2022-03-04 03:41:47 +0100

    Fixed tile bug

--- player.mjs
+++ player.mjs
@@ -1,6 +1,16 @@
 import { Card } from './card.mjs';
 import { tileOwner } from './tile.mjs'
 
+
+class QItem {
+
+  constructor(x, y, w) {
+    this.row = x;
+    this.col = y;
+    this.dist = w;
+  }
+};
+
 class Player {
   constructor(world, jsonObj) {
     this.world = world;
@@ -30,10 +40,11 @@ class Player {
     this.tiles = [];
 
     // updatovati vlasnistvo svakog tilea
-    for (let tile in jsonObj.tiles) {
-      let wTile = this.world.getTile(jsonObj.tiles[tile].x, jsonObj.tiles[tile].y);
-      wTile.owner = tileOwner;
-      this.tiles.push(wTile);
+    for (let i in jsonObj.tiles) {
+      let tile = this.world.getTile(jsonObj.tiles[i].x, jsonObj.tiles[i].y);
+      tile.owner = tileOwner;
+      tile.update(jsonObj.tiles[i]);
+      this.tiles.push(tile);
     }
 
     // ocisti sve kartice i ubaci nove
@@ -54,13 +65,75 @@ class Player {
   }
 
   // owned!
-  getCardCount(cardId)
-  {
-    for(let i in this.cards)
-      if(this.cards[i].cardId == cardId)
+  getCardCount(cardId) {
+    for (let i in this.cards)
+      if (this.cards[i].cardId == cardId)
         return this.cards[i].owned;
     return -1;
   }
+
+  getClosest()
+  {
+    var source = new QItem(0, 0, 0);
+  
+    // To keep track of visited QItems. Marking
+    // blocked cells as visited.
+    var visited = Array.from(Array(8), ()=>Array(8).fill(0));
+    for (var i = 0; i < 8; i++) 
+    {
+        for (var j = 0; j < 8; j++)
+        {
+            let index = i * 8 + j;
+            if (grid[index])
+                visited[i][j] = true;
+            else
+                visited[i][j] = false;
+        }
+    }
+  
+    // applying BFS on matrix cells starting from source
+    var q = [];
+    q.push(source);
+    visited[source.row][source.col] = true;
+    while (q.length!=0) {
+        var p = q[0];
+        q.shift();
+  
+        // Destination found;
+        if (grid[p.row][p.col] == 'd')
+            return p.dist;
+  
+        // moving up
+        if (p.row - 1 >= 0 &&
+            visited[p.row - 1][p.col] == false) {
+            q.push(new QItem(p.row - 1, p.col, p.dist + 1));
+            visited[p.row - 1][p.col] = true;
+        }
+  
+        // moving down
+        if (p.row + 1 < N &&
+            visited[p.row + 1][p.col] == false) {
+            q.push(new QItem(p.row + 1, p.col, p.dist + 1));
+            visited[p.row + 1][p.col] = true;
+        }
+  
+        // moving left
+        if (p.col - 1 >= 0 &&
+            visited[p.row][p.col - 1] == false) {
+            q.push(new QItem(p.row, p.col - 1, p.dist + 1));
+            visited[p.row][p.col - 1] = true;
+        }
+  
+         // moving right
+        if (p.col + 1 < M &&
+            visited[p.row][p.col + 1] == false) {
+            q.push(new QItem(p.row, p.col + 1, p.dist + 1));
+            visited[p.row][p.col + 1] = true;
+        }
+    }
+    return -1;
+  }
+
 }
 
 export { Player };
